413aff07ac85428f31606fd940f22f6c1d6985dd
nathakits <nathakit.tan@gmail.com>
2019-07-30 15:51:06 +0700

    move nav links to header - removed extension bar - removed styles

--- src/.vuepress/theme/components/Header.vue
+++ src/.vuepress/theme/components/Header.vue
@@ -1,46 +1,25 @@
 <template lang="pug">
 
   div
-
     v-toolbar.elevation-1(
       app
       fixed
       clipped-left
       height="60px"
     )
-      SidebarButton(
-        v-if="!$page.frontmatter.home"
-        @toggle-sidebar="$emit('toggle-sidebar')"
-      )
-      router-link.pl-logo(to="/")
-        img(
-          :src="$withBase('/pl_logo.svg')"
-        )
-
-      template(v-slot:extension)
-        div.fill-height.d-flex.justify-center.align-center(style="width:100%;")
-          template(v-for="item in docs_menu")
-            v-btn.my-0(
-              dark
-              flat
-              round
-              :key="item.title"
-              :to="item.link"
-              :class="{'text-capitalize': item.title != 'vr/ar', 'text-uppercase': item.title == 'vr/ar' }"
-              style="max-width:110px;"
-            ) {{ item.title }}
-
-    //- div.pageLinks.hidden-md-and-down
-    //-   div.fill-height.d-flex.justify-center
-    //-     template(v-for="item in docs_menu")
-    //-       v-btn.fill-height(
-    //-         flat
-    //-         :key="item.title"
-    //-         :to="item.link"
-    //-         :class="{'text-capitalize': item.title != 'vr/ar', 'text-uppercase': item.title == 'vr/ar' }"
-    //-         style="margin:0;"
-    //-       ) {{ item.title }}
-
+      SidebarButton(@toggle-sidebar="$emit('toggle-sidebar')")
+      router-link.align-center.d-flex(to="/")
+        img(:src="$withBase('/pl_logo.svg')")
+      v-spacer
+      v-toolbar-items.hidden-md-and-down
+        template(v-for="item in docs_menu")
+          v-btn(
+            flat
+            :key="item.title"
+            :to="item.link"
+            :class="{'text-capitalize': item.title != 'vr/ar', 'text-uppercase': item.title == 'vr/ar' }"
+            style="margin:0;"
+          ) {{ item.title }}
 
 </template>
 
@@ -64,15 +43,3 @@ export default {
   }
 };
 </script>
-
-<style lang="stylus">
-.pageLinks {
-  z-index: 4;
-  position: fixed;
-  left: 0;
-  right: 0
-  max-width: 800px
-  margin: auto;
-  height:60px;
-}
-</style>
