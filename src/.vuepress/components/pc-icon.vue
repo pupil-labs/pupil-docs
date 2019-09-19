9d338847510aafe2475af1547382e880bf1194e1
nathakits <nathakit.tan@gmail.com>
2019-09-19 12:21:01 +0700

    add icon components

---
+++ src/.vuepress/components/pc-icon.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="display:flex;justify-content:center;" class="pb-4">
+    <img src="../../media/core/pc.svg" width="100" />
+  </div>
+</template>
---
+++ src/.vuepress/components/ps-icon.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="display:flex;justify-content:center;" class="pb-4">
+    <img src="../../media/core/ps.svg" width="100" />
+  </div>
+</template>
