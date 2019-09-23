c112ede1e366d67ee01742481866c3508cbbe0d9
nathakits <nathakit.tan@gmail.com>
2019-09-23 12:26:09 +0700

    update algolia component to latest default theme - convert from html to pug

--- src/.vuepress/theme/components/AlgoliaSearchBox.vue
+++ src/.vuepress/theme/components/AlgoliaSearchBox.vue
@@ -1,62 +1,81 @@
-<template>
-  <form
+<template lang="pug">
+  form(
     id="search-form"
     class="algolia-search-wrapper search-box"
     role="search"
-  >
-    <input
+  )
+    input(
       id="algolia-search-input"
       class="search-query"
-    >
-  </form>
+      :placeholder="placeholder"
+    )
 </template>
 
 <script>
 export default {
-  props: ['options'],
+  props: ["options"],
 
-  mounted () {
-    this.initialize(this.options, this.$lang)
+  data() {
+    return {
+      placeholder: undefined
+    };
+  },
+
+  mounted() {
+    this.initialize(this.options, this.$lang);
+    this.placeholder = this.$site.themeConfig.searchPlaceholder || "";
   },
 
   methods: {
-    initialize (userOptions, lang) {
+    initialize(userOptions, lang) {
       Promise.all([
-        import(/* webpackChunkName: "docsearch" */ 'docsearch.js/dist/cdn/docsearch.min.js'),
-        import(/* webpackChunkName: "docsearch" */ 'docsearch.js/dist/cdn/docsearch.min.css')
+        import(
+          /* webpackChunkName: "docsearch" */ "docsearch.js/dist/cdn/docsearch.min.js"
+        ),
+        import(
+          /* webpackChunkName: "docsearch" */ "docsearch.js/dist/cdn/docsearch.min.css"
+        )
       ]).then(([docsearch]) => {
-        docsearch = docsearch.default
-        const { algoliaOptions = {}} = userOptions
-        docsearch(Object.assign(
-          {},
-          userOptions,
-          {
-            inputSelector: '#algolia-search-input',
+        docsearch = docsearch.default;
+        const { algoliaOptions = {} } = userOptions;
+        docsearch(
+          Object.assign({}, userOptions, {
+            inputSelector: "#algolia-search-input",
             // #697 Make docsearch work well at i18n mode.
-            algoliaOptions: Object.assign({
-              'facetFilters': [`lang:${lang}`].concat(algoliaOptions.facetFilters || [])
-            }, algoliaOptions)
-          }
-        ))
-      })
+            algoliaOptions: Object.assign(
+              {
+                facetFilters: [`lang:${lang}`].concat(
+                  algoliaOptions.facetFilters || []
+                )
+              },
+              algoliaOptions
+            ),
+            handleSelected: (input, event, suggestion) => {
+              const { pathname, hash } = new URL(suggestion.url);
+              this.$router.push(`${pathname}${hash}`);
+            }
+          })
+        );
+      });
     },
 
-    update (options, lang) {
-      this.$el.innerHTML = '<input id="algolia-search-input" class="search-query">'
-      this.initialize(options, lang)
+    update(options, lang) {
+      this.$el.innerHTML =
+        '<input id="algolia-search-input" class="search-query">';
+      this.initialize(options, lang);
     }
   },
 
   watch: {
-    $lang (newValue) {
-      this.update(this.options, newValue)
+    $lang(newValue) {
+      this.update(this.options, newValue);
     },
 
-    options (newValue) {
-      this.update(newValue, this.$lang)
+    options(newValue) {
+      this.update(newValue, this.$lang);
     }
   }
-}
+};
 </script>
 
 <style lang="stylus">
