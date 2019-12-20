2540f015e3cdb75b231fa167d84bd3f4a55e4b0e
Caspar Goeke <caspar.goeke@gmail.com>
2019-12-20 14:48:48 +0700

    make re-directs

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -21,7 +21,7 @@ import Vuetify, {
   VListTileTitle
 } from 'vuetify/lib';
 
-export default ({ Vue }) => {
+export default ({ Vue, router, options }) => {
   Vue.use(Vuetify, {
     components: {
       VApp,
@@ -49,4 +49,23 @@ export default ({ Vue }) => {
       primary: "#1263CC"
     }
   });
+
+  var redirect_dict = {
+    '/#export' :'/core/software/pupil-player/#export',
+    '/#surface-tracking' : '/core/software/pupil-capture/#surface-tracking',
+    '/#analysis-plugins' : '/core/software/pupil-player/#analysis-plugins',
+    '/#pupil-capture' :'/core/software/pupil-capture/',
+    '/#pupil-player' : '/core/software/pupil-player/',
+    '/#network-plugins' : '/core/software/pupil-capture/#network-plugins',
+    '/#interprocess-and-network-communication' : '/developer/core/network-api/',
+    '/#annotations' : '/core/software/pupil-capture/#annotations'
+  }
+
+  router.beforeEach((to, from, next) => {
+    if (Object.keys(redirect_dict).indexOf(to.fullPath)>=0){
+      next(redirect_dict[to.fullPath])
+    } else{
+      next()
+    }
+  })
 };
