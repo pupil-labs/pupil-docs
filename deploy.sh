e1d6e677331278d3980714cf6d442d7d055ab5a9
Will Patera <willpatera@gmail.com>
2020-03-17 12:35:18 +0700

    WIP update travis-ci only to deploy when master brach

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+echo "Deploying to production"
+eval "$(ssh-agent -s)"
+chmod 600 ./docs_deploy_key
+echo -e "Host $SERVER_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./docs_deploy_key
+rsync -hrvz --delete --exclude '.htaccess' -e 'ssh -i ./docs_deploy_key -o "StrictHostKeyChecking no"' src/.vuepress/dist/ ${SERVER_USER}@${SERVER_IP}:~/domains/docs.pupil-labs.com/html/
+echo "Production deployed"
