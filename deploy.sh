d4d02d5f2d64dce709bf331e99e878697f597666
Will Patera <willpatera@gmail.com>
2020-03-17 12:41:16 +0700

    WIP debugging travis pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo "Deploying to production"
 eval "$(ssh-agent -s)"
-chmod 600 ./docs_deploy_key
+chmod 600 ./docs_deploy_key.enc
 echo -e "Host $SERVER_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh-add ./docs_deploy_key
+ssh-add ./docs_deploy_key.enc
 rsync -hrvz --delete --exclude '.htaccess' -e 'ssh -i ./docs_deploy_key -o "StrictHostKeyChecking no"' src/.vuepress/dist/ ${SERVER_USER}@${SERVER_IP}:~/domains/docs.pupil-labs.com/html/
 echo "Production deployed"
