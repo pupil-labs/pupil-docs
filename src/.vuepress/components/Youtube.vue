b3ccccadf621ef553039a74026aa0209633571a9
Will Patera <willpatera@gmail.com>
2020-06-16 16:28:17 +0700

    Set autoplay and rel to 0 related will still play videos from our channel, but at least it will not play something random from the internet

--- src/.vuepress/components/Youtube.vue
+++ src/.vuepress/components/Youtube.vue
@@ -3,9 +3,9 @@
     iframe(
       width="560"
       height="315"
-      :src="`https://www.youtube-nocookie.com/embed/${src}`"
+      :src="`https://www.youtube-nocookie.com/embed/${src}?autoplay=0&rel=0`"
       frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen
     )
 </template>
