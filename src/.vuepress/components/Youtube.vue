55e588e2a6ab182589b5deda483997b2090de945
Will Patera <willpatera@gmail.com>
2020-07-01 11:59:28 +0700

    Hack to make YT embeds wider

--- src/.vuepress/components/Youtube.vue
+++ src/.vuepress/components/Youtube.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   div(style="display:flex;justify-content:center;").pb-4
     iframe(
-      width="560"
-      height="315"
+      width="800"
+      height="450"
       :src="`https://www.youtube-nocookie.com/embed/${src}?autoplay=0&rel=0&modestbranding=1&loop=1`"
       frameborder="0"
       allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
