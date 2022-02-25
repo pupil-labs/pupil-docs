32d5297348450fb08e3f7a4be4db4f0a898e1eee
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2022-02-25 15:46:31 +0700

    update sidebar link - wrap sidebar links with span - try to center align sidebar text - add margin to outbound icon

--- src/.vuepress/theme/components/SidebarLink.vue
+++ src/.vuepress/theme/components/SidebarLink.vue
@@ -70,7 +70,7 @@ function renderLink(h, to, text, active) {
         "sidebar-link": true,
       },
     },
-    text
+    [h("span", text)]
   );
 }
 
@@ -114,7 +114,7 @@ function renderExternal(h, to, text) {
         "sidebar-link": true,
       },
     },
-    [text, h("OutboundLink")]
+    [h("span", text), h("OutboundLink")]
   );
 }
 </script>
@@ -146,6 +146,11 @@ a.sidebar-link {
   width: 100%;
   box-sizing: border-box;
   min-height: 24px;
+  line-height: 1;
+
+  > span {
+    transform: translateY(1px);
+  }
 
   &:hover {
     color: #0D122A;
@@ -255,5 +260,10 @@ li > a.sidebar-link {
 
 .sidebar-link.active {
   color: #1263cc;
+}
+
+.icon.outbound {
+  margin-left: 4px;
+  top: 0;
 }
 </style>
