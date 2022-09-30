1e1310a5f1bedd422da33739806927a6be8dc791
Miguel García García <mgg@Miguels-MacBook-Air.local>
2022-09-30 16:26:28 +0200

    Changed PhotoGrid to look better (more responsible)

--- src/.vuepress/components/PhotoGrid.vue
+++ src/.vuepress/components/PhotoGrid.vue
@@ -21,17 +21,50 @@ export default {
 <style>
 .gallery {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-  grid-gap: 1rem;
-  max-width: 80rem;
-  margin: 5rem auto;
-  padding: 0 5rem;
+  grid-template-columns: repeat(auto-fill, 24%);
+  grid-gap: 0.5%;
+  max-width: 100%;
+  margin: 0rem 0rem;
+  padding: 0 0px;
 }
 
 .gallery-panel img {
   width: 100%;
-  height: 22vw;
+  height: 15vw;
   object-fit: cover;
   border-radius: 0.75rem;
+  background-color: rgb(245,245,245);
 }
+
+
+@media screen and (min-width: 1025px) and (max-width: 1200px) {
+    .gallery {
+    grid-template-columns: repeat(auto-fill, 48%);
+    }
+    .gallery-panel img {
+    height: 20vw;
+    }
+}
+
+@media screen and (max-width: 800px){
+    .gallery {
+    grid-template-columns: repeat(auto-fill, 49%);
+    }
+    .gallery-panel img {
+    object-fit:contain;
+    height: 30vw;
+    }
+}
+
+/* One column for mobiles */
+@media screen and (max-width: 400px) {
+    .gallery {
+    grid-template-columns: repeat(auto-fill, 100%);
+    }
+    .gallery-panel img {
+    height: 100%;
+    }
+}
+
+
 </style>
