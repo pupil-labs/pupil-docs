2a0a5fe4925437ee0a116ebad3e8c10a257ad757
Miguel García García <mikelgg93@gmail.com>
2022-10-28 13:10:44 +0200

    Added tags for products
    A  src/.vuepress/components/AllTags.vue
    A  src/.vuepress/components/TagLinks.vue
    M  src/invisible/the-forge/map-your-gaze-to-a-2d-screen.md
    A  src/tags.md

---
+++ src/.vuepress/components/TagLinks.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div>
+    <router-link
+      v-for="tag in $page.frontmatter.tags"
+      :key="tag"
+      :to="{ path: getPageLink(tag) }"
+    >
+        {{ tag }}  
+    </router-link>
+    <br></br>
+  </div>
+</template>
+<script>
+export default {
+    methods: {
+        getPageLink(tag) {
+            if (tag.toLowerCase().includes('invisible')) {
+                return '/invisible/'
+            }
+            else if (tag.toLowerCase().includes('cloud')) {
+                return '/invisible/getting-started/analyse-recordings-in-pupil-cloud/'
+            }
+            else if (tag.toLowerCase().includes('core')) {
+                return '/core/'
+            }
+            else {
+                return '/tags.html'
+            }
+        }
+    },
+}
+</script>
