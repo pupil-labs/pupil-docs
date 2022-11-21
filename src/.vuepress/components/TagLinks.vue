2ccc1f9e72c5f2f3eba3d9538fc1c9bddd4c8aa9
Nathakit Sae-Tan <nat@pupil-labs.com>
2022-11-21 17:52:06 +0700

    fix comma bug

--- src/.vuepress/components/TagLinks.vue
+++ src/.vuepress/components/TagLinks.vue
@@ -1,33 +1,43 @@
-<template lang="html">
+<template>
   <div class="caption--1 pb-3">
-    <span>Products used: </span>
-    <a
-      v-for="(tag, index) in $page.frontmatter.tags"
-      :key="tag"
-      :href="getPageLink(tag)"
-      color="primary"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {{ index + 1 < $page.frontmatter.tags.length ? `${tag}, ` : tag }}
-    </a>
+    <span class="">Products used:</span>
+    <span class="taglinks">
+      <template v-for="(tag, index) in $page.frontmatter.tags">
+        <a
+          :key="`${tag}-${index}`"
+          :href="getPageLink(tag)"
+          color="primary"
+          target="_blank"
+          rel="noopener noreferrer"
+          >{{ tag }}</a
+        >
+      </template>
+    </span>
   </div>
 </template>
 <script>
-export default {
-  methods: {
-    getPageLink(tag) {
-      const url = "https://pupil-labs.com/products";
-      if (tag.toLowerCase().includes("invisible")) {
-        return `${url}/invisible/`;
-      } else if (tag.toLowerCase().includes("cloud")) {
-        return `${url}/cloud`;
-      } else if (tag.toLowerCase().includes("core")) {
-        return `${url}/core/`;
-      } else {
-        return;
-      }
-    }
-  }
-};
+  export default {
+    methods: {
+      getPageLink(tag) {
+        const url = "https://pupil-labs.com/products";
+        if (tag.toLowerCase().includes("invisible")) {
+          return `${url}/invisible/`;
+        } else if (tag.toLowerCase().includes("cloud")) {
+          return `${url}/cloud/`;
+        } else if (tag.toLowerCase().includes("core")) {
+          return `${url}/core/`;
+        } else {
+          return;
+        }
+      },
+    },
+  };
 </script>
+
+<style scoped>
+  .taglinks a:not(:first-child):before {
+    font-family: AtlasGrotesk, sans-serif;
+    font-size: 12px !important;
+    content: ", ";
+  }
+</style>
