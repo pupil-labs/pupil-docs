abdb032aee0cc091070a550110e93031764ddd1a
Marc Tonsen <mar@pupil-labs.com>
2023-11-02 14:47:02 +0100

    Add custom components again

---
+++ neon/.vitepress/components/Youtube.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.pb-4(style="display: flex; justify-content: center; aspect-ratio: 16 / 9")
+  iframe(
+    width="100%",
+    height=height,
+    :src="`https://www.youtube-nocookie.com/embed/${src}?autoplay=${autoplay}&rel=0&modestbranding=1&loop=1&muted=${muted}&cc_load_policy=${closed_caption}`",
+    frameborder="0",
+    allow="accelerometer; encrypted-media; gyroscope; picture-in-picture",
+    allowfullscreen
+  )
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: String,
+      default: "100%",
+    },
+    width: {
+      type: String,
+      default: "100%",
+    },
+    muted: {
+      type: String,
+      default: "0",
+    },
+    autoplay: {
+      type: String,
+      default: "0",
+    },
+    closed_caption: {
+      type: String,
+      default: "0",
+    },
+  },
+};
+</script>
