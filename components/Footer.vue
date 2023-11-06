6d0496dd99af8a6328c00ec4e0c1cb55ff23a595
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-11-06 15:24:23 +0700

    update footer link

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,7 +7,7 @@
     <div
       class="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4"
     >
-      <div><PupilLogo /></div>
+      <a href="https://docs-staging.pupil-labs.com"><PupilLogo /> </a>
       <div class="text-sm">
         <div>info@pupil-labs.com</div>
         <div>Copyright 2023 Pupil Labs GmbH. All rights reserved.</div>
