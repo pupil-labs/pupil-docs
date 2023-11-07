ce4445190775f96017cf08094ed16f4b97d6b5f7
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-11-07 16:35:10 +0700

    fix tailwindcss missing css bug - make sure to add global components path to config

--- neon/tailwind.config.js
+++ neon/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.md", "./.vitepress/theme/**/*.{vue,js,ts}"],
+  content: [
+    "./**/*.md",
+    "./.vitepress/theme/**/*.{vue,js,ts}",
+    "../components/**/*.vue",
+  ],
   theme: {
     extend: {
       screens: {
