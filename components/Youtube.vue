925c570d4c4f8eff24f41ac0fc6b2a0410a8b940
Miguel García García <mikelgg93@gmail.com>
2025-02-27 11:55:08 +0100

    feat: Update component with latest controls allowed by YT

--- components/Youtube.vue
+++ components/Youtube.vue
@@ -1,22 +1,29 @@
 <script setup lang="ts">
-  interface Props {
-    src: string;
-    height: string;
-    width: string;
-    muted: string;
-    autoplay: string;
-    closedCaption: string;
-  }
+interface Props {
+  src: string;
+  height: string;
+  width: string;
+  muted: string;
+  autoplay: string;
+  closedCaption: string;
+  color?: "red" | "white";
+  controls?: "0" | "1";
+  iv_load_policy?: "1" | "3";
+  loop?: "0" | "1";
+  list?: string;
+}
 
-  const { src, muted, autoplay, closedCaption } = withDefaults(
-    defineProps<Props>(),
-    {
-      height: "auto",
-      muted: "1",
-      autoplay: "0",
-      closedCaption: "1",
-    }
-  );
+const { src, muted, autoplay, closedCaption } = withDefaults(
+  defineProps<Props>(),
+  {
+    height: "auto",
+    muted: "1",
+    autoplay: "0",
+    closedCaption: "1",
+    iv_load_policy: "1",
+    loop: "1",
+  }
+);
 </script>
 
 <template>
@@ -24,7 +31,7 @@
     <iframe
       width="100%"
       height="height"
-      :src="`https://www.youtube-nocookie.com/embed/${src}?autoplay=${autoplay}&rel=0&modestbranding=1&loop=1&muted=${muted}&cc_load_policy=${closedCaption}`"
+      :src="`https://www.youtube-nocookie.com/embed/${src}?autoplay=${autoplay}&rel=0&loop=${loop}&muted=${muted}&cc_load_policy=${closedCaption}&controls=${controls}&iv_load_policy=${iv_load_policy}&rel=0&enablejsapi=1`"
       frameborder="0"
       allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen
